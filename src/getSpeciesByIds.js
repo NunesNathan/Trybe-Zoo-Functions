637e18577b52d0c1d46e832eb5451de9f2893f41
NunesNathan <nunesxd7@hotmail.com>
2021-10-16 18:14:49 -0300

    C1 - requisito 1

--- src/getSpeciesByIds.js
+++ src/getSpeciesByIds.js
@@ -1,7 +1,8 @@
 const data = require('../data/zoo_data');
 
-function getSpeciesByIds(ids) {
-  // seu código aqui
+function getSpeciesByIds(...ids) {
+  const { species } = data;
+  return species.filter((race) => ids.some((id) => race.id === id));
 }
 
 module.exports = getSpeciesByIds;
