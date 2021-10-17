c0f02d58f261ef5404992051bd4eec4720ee5250
NunesNathan <nunesxd7@hotmail.com>
2021-10-17 00:21:41 -0300

    C4 - requisito 4

--- src/getRelatedEmployees.js
+++ src/getRelatedEmployees.js
@@ -1,11 +1,19 @@
 const data = require('../data/zoo_data');
 
+const { employees } = data;
+
 function isManager(id) {
-  // seu código aqui
+  return employees.some((employee) => employee.managers.includes(id));
 }
 
 function getRelatedEmployees(managerId) {
-  // seu código aqui
+  if (isManager(managerId)) {
+    const result = [];
+    employees.filter((sameOfId) => sameOfId.managers.includes(managerId))
+      .forEach((each) => result.push(`${each.firstName} ${each.lastName}`));
+    return result;
+  }
+  throw new Error('O id inserido não é de uma pessoa colaboradora gerente!');
 }
 
 module.exports = { isManager, getRelatedEmployees };
