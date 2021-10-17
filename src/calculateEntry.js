15b7f9facdc659768132f0e881469f7f217bd0a5
NunesNathan <nunesxd7@hotmail.com>
2021-10-17 17:23:06 -0300

    C5 - requisito 6

--- src/calculateEntry.js
+++ src/calculateEntry.js
@@ -1,11 +1,25 @@
 const data = require('../data/zoo_data');
 
 function countEntrants(entrants) {
-  // seu código aqui
+  const child = entrants.filter((kid) => kid.age < 18).length;
+  const adult = entrants.filter((kid) => kid.age >= 18 && kid.age < 50).length;
+  const senior = entrants.filter((kid) => kid.age >= 50).length;
+
+  return { child, adult, senior };
 }
 
 function calculateEntry(entrants) {
-  // seu código aqui
+  if (entrants === undefined || entrants[0] === undefined) {
+    return 0;
+  }
+
+  const { prices } = data;
+  const result = countEntrants(entrants);
+  const childMoney = result.child * prices.child;
+  const adultMoney = result.adult * prices.adult;
+  const seniorMoney = result.senior * prices.senior;
+  const total = childMoney + adultMoney + seniorMoney;
+  return total;
 }
 
 module.exports = { calculateEntry, countEntrants };
