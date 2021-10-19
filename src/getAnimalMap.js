30cb93cbdf3959e4aa97b3c0be0234e5a23b0f65
NunesNathan <nunesxd7@hotmail.com>
2021-10-19 00:49:58 -0300

    C6 - requisito 8

--- src/getAnimalMap.js
+++ src/getAnimalMap.js
@@ -1,7 +1,17 @@
 const data = require('../data/zoo_data');
 
 function getAnimalMap(options) {
-  // seu código aqui
+  const { species } = data;
+  const solveCallback = (shortLocate) => species
+    .filter((specie) => specie.location === shortLocate).map((index) => index.name);
+  const NE = solveCallback('NE');
+  const NW = solveCallback('NW');
+  const SE = solveCallback('SE');
+  const SW = solveCallback('SW');
+  const key = { NE, NW, SE, SW };
+
+  return key;
 }
 
+getAnimalMap({ includeNames: true });
 module.exports = getAnimalMap;
--- src/getSchedule.js
+++ src/getSchedule.js
@@ -1,7 +1,64 @@
 const data = require('../data/zoo_data');
 
+const { species } = data;
+const { hours } = data;
+
+const arrayAnimals = (param) => {
+  const arrA = [];
+  species.forEach((specie) => {
+    if (specie.availability.includes(param)) {
+      arrA.push(specie.name);
+    }
+  });
+  return arrA;
+};
+
+const makeObj = (param) => {
+  const myobj = {};
+  (Object.entries(hours).forEach((hour) => {
+    if (param === 'Monday') {
+      myobj.officeHour = 'CLOSED';
+      myobj.exhibition = 'The zoo will be closed!';
+    } else if (hour[0] === param) {
+      myobj.officeHour = `Open from ${hour[1].open}am until ${hour[1].close}pm`;
+      myobj.exhibition = arrayAnimals(param);
+    }
+  }));
+  return myobj;
+};
+
+const hoursOn = () => {
+  const result = (Object.entries(hours).map((day) => day[0])
+    .map((nday) => ({ nday }))).reduce((acc, obj) => {
+    acc[obj.nday] = makeObj(obj.nday);
+    return acc;
+  }, {});
+  return result;
+};
+
+const searchAnimals = () => {
+  const result = [];
+  species.forEach((animal) => result.push(animal.name));
+  return result;
+};
+
+const week = hoursOn();
+const animalsList = searchAnimals();
+
 function getSchedule(scheduleTarget) {
-  // seu código aqui
+  let result = {};
+  if (Object.entries(week).find((day) => day[0] === scheduleTarget)) {
+    result[`${scheduleTarget}`] = week[`${scheduleTarget}`];
+    return result;
+  } if (animalsList.includes(scheduleTarget)) {
+    species.forEach((animal) => {
+      if (animal.name === scheduleTarget) {
+        result = animal.availability;
+      }
+    });
+    return result;
+  }
+  return week;
 }
 
 module.exports = getSchedule;
