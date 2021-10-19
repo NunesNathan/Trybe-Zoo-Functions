83ff8261732ea4fa8c5855953b4b2f41cc88984c
NunesNathan <nunesxd7@hotmail.com>
2021-10-19 20:45:34 -0300

    C7 - requisito 9

--- src/getOldestFromFirstSpecies.js
+++ src/getOldestFromFirstSpecies.js
@@ -1,7 +1,18 @@
 const data = require('../data/zoo_data');
 
+const { species } = data;
+const { employees } = data;
+
+const animalsFunc = (param) => {
+  const animal = species.find((animal) => animal.id === param);
+  const result = animal.residents.sort((a, b) => b.age - a.age)[0];
+  return (Object.values(result));
+};
+
 function getOldestFromFirstSpecies(id) {
-  // seu código aqui
+  let employee = employees.find((employy) => id === employy.id);
+  employee = employee.responsibleFor;
+  return animalsFunc(employee[0]);
 }
 
 module.exports = getOldestFromFirstSpecies;
