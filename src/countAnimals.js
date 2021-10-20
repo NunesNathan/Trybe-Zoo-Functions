562aee519a4678bcc6ee96cd1d75d4f81da16b78
NunesNathan <nunesxd7@hotmail.com>
2021-10-19 21:47:26 -0300

    C8 - requisito 5

--- src/countAnimals.js
+++ src/countAnimals.js
@@ -1,7 +1,32 @@
 const data = require('../data/zoo_data');
 
+const { species } = data;
+
+const voidParam = () => {
+  const myObj = {};
+  species.forEach((specie) => { myObj[`${specie.name}`] = Number(`${specie.residents.length}`); });
+  return myObj;
+};
+
+const withSpecie = (param) => {
+  const specime = species.find((specier) => specier.name === param.specie);
+  return specime.residents;
+};
+
+const withSexToo = (param) => {
+  let result = withSpecie(param);
+  result = result.filter((findSex) => findSex.sex === param.sex);
+  return result;
+};
+
 function countAnimals(animal) {
-  // seu código aqui
+  if (animal === undefined) {
+    return voidParam();
+  }
+  if (animal.sex !== undefined) {
+    return withSexToo(animal).length;
+  }
+  return withSpecie(animal).length;
 }
 
 module.exports = countAnimals;
